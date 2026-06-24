1e0391dd3999d77c657a9e38068a817e2f666fda
Amirhossein Alian <alianmail81@gmail.com>
2026-06-24 20:54:04 +0330

    updated script and add build script to package.json

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/amir/w/web-termux-app && git pull
-npx @tailwindcss/cli -i /home/amir/w/web-termux-app/ui/input.css -o /home/amir/w/web-termux-app/ui/output.css
+/home/amir/.local/share/fnm/node-versions/v24.18.0/installation/lib/node_modules/npm/bin/npm-cli.js run build:css
+
